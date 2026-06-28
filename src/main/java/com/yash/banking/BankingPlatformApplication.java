f4474a6dd7460db1242b03a4783292b13d8c96b0
yash <yashwable12@gmail.com>
2026-06-28 16:34:51 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/yash/banking/BankingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.yash.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yash/banking/BankingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
