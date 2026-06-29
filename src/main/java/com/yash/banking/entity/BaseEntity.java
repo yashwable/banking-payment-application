7dffa8a83fda6d20ad501f7e9b90ca9926c586a6
yash <yashwable12@gmail.com>
2026-06-30 00:21:50 +0530

    feat: add role entity

--- src/main/java/com/yash/banking/entity/BaseEntity.java
+++ src/main/java/com/yash/banking/entity/BaseEntity.java
@@ -1,12 +1,31 @@
 package com.yash.banking.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
-    UUID id;
 
-    LocalDateTime createdAt;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
-    LocalDateTime updatedAt;
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
---
+++ src/main/java/com/yash/banking/entity/Role.java
@@ -0,0 +1,22 @@
+package com.yash.banking.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role extends BaseEntity {
+
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(name = "description", length = 255)
+    private String description;
+}
